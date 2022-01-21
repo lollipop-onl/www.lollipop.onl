62278cf3d6f2a44a3baf5f748950f5910bd8f719
lollipop-onl <lollipop@simochee.net>
2022-01-21 18:58:34 +0900

    🛁 Cleanup repo

--- plugins/libs/dayjs.ts
+++
@@ -1,9 +0,0 @@
-import { Plugin } from '@nuxt/types';
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
-
-const plugin: Plugin = (): void => {
-  dayjs.extend(relativeTime);
-};
-
-export default plugin;
